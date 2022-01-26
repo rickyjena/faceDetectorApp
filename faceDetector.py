4dae88b2f32cedd90f9a13ddb9f4476d7e27a237
rickyjena <ricky.jena@gmail.com>
2022-01-25 18:19:45 -0800

    grayscaled images for detection

--- faceDetector.py
+++ faceDetector.py
@@ -6,8 +6,11 @@ trainedFaceData = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Image sample to detect face
 img = cv2.imread('243315131_6485777301463164_7029905873815792864_n.jpg')
 
+# We will convert image to grayscale for detector requirements
+grayScaleImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 # Show sample image
-cv2.imshow('Face Detector', img)
+cv2.imshow('Face Detector', grayScaleImg)
 cv2.waitKey()
 
 print("my Face")
